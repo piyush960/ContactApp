f823ad5f296041be4f8eb1675f79712006fa5ab8
piyush960 <piyushdahake096@gmail.com>
2024-11-16 04:03:06 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-
-  plugins: [react()],
-
-  
-  })
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      VITE_REACT_ENV: process.env.VITE_REACT_ENV,
+    },
+  }  
+})
